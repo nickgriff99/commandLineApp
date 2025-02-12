db20aad1d779da8e61560b044bb439c72fb9d304
Nicholas Griffiths <nichg999@gmail.com>
2025-02-11 22:35:42 -0700

    finalized interactive package project

--- index.js
+++ index.js
@@ -8,29 +8,49 @@ var rs = require('readline-sync');
 // print out list of hobbies at the end
 
 var hobbies = [];
+var areYouSure = false;
+var moreHobbies = 0;
+
+function getHobbyCount() {
+  moreHobbies = rs.questionInt('How many additional hobbies do you have? ');
+  return moreHobbies;
+}
+
+function addHobbies(numHobbies) {
+  var counter = 0;
+  while(counter < numHobbies) {
+    var input = rs.prompt();
+    hobbies.push(input);
+    counter++;
+
+    if (counter < moreHobbies) {
+      console.log('-- Awesome! "' + input + '" is fun! What\'s the other ' + (numHobbies - counter) + ' hobby?');
+    } else {
+      console.log('I wish I could do ' + hobbies + '... but I\'m just a computer.');
+    }
+  }
+}
+
 
 var userName = rs.question('May I have your name? ');
 
 var favHobby = rs.question(userName + ', what is your favorite hobby? ');
 
 hobbies.push(favHobby);
+  getHobbyCount();
 
-var moreHobbies = rs.questionInt('How many additional hobbies do you have? ');
 
 if (moreHobbies > 0) {
-  var counter = 0;
-  while(counter < moreHobbies) {
-    var input = rs.prompt();
-    hobbies.push(input);
-    counter++;
-    if (counter < moreHobbies) {
-      console.log('-- Awesome! "' + input + '" is fun! What\'s the other ' + (moreHobbies - counter) + ' hobby?');
-    } else {
-      console.log('I wish I could do ' + hobbies + '... but I\'m just a computer.');
-    }
-  }
+  console.log('Cool, what are they? ')
+  addHobbies(moreHobbies);
 } else {
-  var areYouSure = rs.keyInYN('Are you sure? ' + hobbies[0] + ' is all you like to do? ');
+  areYouSure = rs.keyInYN('Are you sure? ' + hobbies[0] + ' is all you like to do? ');
 }
 
-console.log(hobbies);
+if (!areYouSure && hobbies.length <= 1) {
+  getHobbyCount();
+  console.log('Cool, what are they? ');
+  addHobbies(moreHobbies);
+} else if (areYouSure) {
+  console.log(hobbies + ' is cool, good for you. Goodbye!');
+}

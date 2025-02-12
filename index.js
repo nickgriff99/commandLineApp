e83f19e14373e043948bfd3e972b0fb7f9404196
Nicholas Griffiths <nichg999@gmail.com>
2025-02-11 22:16:21 -0700

    added hobbies questions and logic

--- index.js
+++ index.js
@@ -11,7 +11,26 @@ var hobbies = [];
 
 var userName = rs.question('May I have your name? ');
 
-var favHobby = rs.question('What is your favorite hobby? ');
+var favHobby = rs.question(userName + ', what is your favorite hobby? ');
 
 hobbies.push(favHobby);
+
+var moreHobbies = rs.questionInt('How many additional hobbies do you have? ');
+
+if (moreHobbies > 0) {
+  var counter = 0;
+  while(counter < moreHobbies) {
+    var input = rs.prompt();
+    hobbies.push(input);
+    counter++;
+    if (counter < moreHobbies) {
+      console.log('-- Awesome! "' + input + '" is fun! What\'s the other ' + (moreHobbies - counter) + ' hobby?');
+    } else {
+      console.log('I wish I could do ' + hobbies + '... but I\'m just a computer.');
+    }
+  }
+} else {
+  var areYouSure = rs.keyInYN('Are you sure? ' + hobbies[0] + ' is all you like to do? ');
+}
+
 console.log(hobbies);
